1df358eda2407f4f74fe66f9df139527db444ae4
Edan Saedi <edsaedi@gmail.com>
2019-02-20 17:35:03 -0800

    I learned about delegates and added extensions.

---
+++ Self-Balancing Trees/RedBlackTree/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RedBlackTree
+{
+    public class RedBlackTree<T>
+    {
+        internal class Node
+        {
+            internal T Value { get; set; }
+            internal Node Right { get; set; }
+            internal Node Left { get; set; }
+
+            internal Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        internal Node Root { get; private set; } = null;
+        public int Count { get; private set; } = 0;
+
+        public RedBlackTree()
+        {
+
+        }
+
+        public void Add(T value)
+        {
+            Add(Root, value);
+            Count++;
+        }
+
+        internal Node Add(Node node, T value)
+        {
+            if (node == null)
+            {
+                return new Node(value);
+            }
+
+            return null;
+        }
+    }
+}
