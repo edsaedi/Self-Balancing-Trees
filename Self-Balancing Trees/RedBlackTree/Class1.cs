43032722cb30e477853c6d318c3c59b674c672a6
Edan Saedi <edsaedi@gmail.com>
2019-03-29 17:32:19 -0700

    Finished the add function for the red black tree. Needs colors and fixup

--- Self-Balancing Trees/RedBlackTree/Class1.cs
+++ Self-Balancing Trees/RedBlackTree/Class1.cs
@@ -1,28 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace RedBlackTree
 {
-    public class RedBlackTree<T>
+    public class RedBlackTree<T> where T : IComparable<T>
     {
-        internal class Node
+        internal class Node : IComparable<Node>
         {
-            internal T Value { get; set; }
-            internal Node Right { get; set; }
-            internal Node Left { get; set; }
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int Height { get; set; }
+            public Node First
+            {
+                get
+                {
+                    if (Left != null)
+                    {
+                        return Left;
+                    }
+                    if (Right != null)
+                    {
+                        return Right;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            public int ChildCount
+            {
+                get
+                {
+                    int count = 0;
+                    if (Left != null)
+                    {
+                        count++;
+                    }
+                    if (Right != null)
+                    {
+                        count++;
+                    }
+                    return count;
+                }
+            }
 
-            internal Node(T value)
+            public int Balance
+            {
+                get
+                {
+                    int right = Right == null ? 0 : Right.Height;
+                    int left = Left == null ? 0 : Left.Height;
+                    return right - left;
+                }
+            }
+
+            public Node(T value)
             {
                 Value = value;
+                Height = 1;
+            }
+
+            public int CompareTo(Node other)
+            {
+                return Value.CompareTo(other.Value);
+            }
+
+            public void PrintPretty(string indent, bool last)
+            {
+
+                Debug.Write(indent);
+                if (last)
+                {
+                    Debug.Write("└─");
+                    indent += "  ";
+                }
+                else
+                {
+                    Debug.Write("├─");
+                    indent += "| ";
+                }
+                Debug.WriteLine(Value);
+
+                List<Node> children = new List<Node>();
+                if (Left != null)
+                {
+                    children.Add(Left);
+                }
+
+                if (Right != null)
+                {
+                    children.Add(Right);
+                }
+
+                for (int i = 0; i < children.Count; i++)
+                {
+                    children[i].PrintPretty(indent, i == children.Count - 1);
+                }
+
             }
         }
 
         internal Node Root { get; private set; } = null;
         public int Count { get; private set; } = 0;
 
-        public RedBlackTree()
-        {
+        //public RedBlackTree()
+        //{
 
-        }
+        //}
 
         public void Add(T value)
         {
@@ -37,7 +124,16 @@ namespace RedBlackTree
                 return new Node(value);
             }
 
-            return null;
+            if (value.CompareTo(node.Value) < 0)
+            {
+                Add(node.Left, value);
+            }
+            else
+            {
+                Add(node.Right, value);
+            }
+
+            return node;
         }
     }
 }
